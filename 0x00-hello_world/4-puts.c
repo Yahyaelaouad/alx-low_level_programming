d8cbc3654d3faa9cd4a5b00a83ece3d693e41096
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-03-16 11:04:22 -0700

    puts 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,13 +6,9 @@
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
-
-
 int main(void)
 
-
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
 }       

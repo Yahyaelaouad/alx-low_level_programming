89f1f217fd6299ec8bfad9bb71e8646a049491eb
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-03-17 15:14:14 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,11 @@
   */
 int main(void)
 {
-	char low, e, q;
-	
-	e = 'e';
-	q = 'q';
+	char low ;
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-	if (low != e && low !=q)
+	if (low != 'e' && low != 'q')
 	putchar(low);
 	}
 	putchar('\n');

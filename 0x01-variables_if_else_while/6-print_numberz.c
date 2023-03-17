9ce2c1dc1c18275a1862453b31921bcd09d9a92d
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-03-17 16:05:22 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,13 @@
   * Return: Always 0 (Success)
   */
 int main(void)
-
 {
-	int d;
+	int i;
 
-	for (i = '0'; d <= '9'; d++)
-	printf(d);
-	printf("\n");
-
-	return (0);
+	for(i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return 0;
 }

e0ac592e7ce47b4f189d0cd67923f416c6307f16
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-03-29 14:33:33 -0700

    strncat 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,4 +18,9 @@ char *_strncat(char *dest, char *src, int n)
 		continue;
 	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
-{
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}

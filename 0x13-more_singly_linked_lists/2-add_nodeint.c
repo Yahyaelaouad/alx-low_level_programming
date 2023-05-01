ef32c0b2c7d38156d1e80f4c24e77321a5fd696e
Yahyaelaouad <yahyaelaouad0@gmail.com git config --global user.email yahyaelaouad0@gmail.com>
2023-05-01 16:49:10 -0700

    TASK 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a node to the beginning of a linked list
- * @head: pointer to the head of the list
- * @n: integer to be used as content
- * Return: address of the newly added node
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to a pointer to the head of the list.
+ * @n: integer value to store in the new node.
+ * Return: address of the new element, or 0 if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
-
-	node = malloc(sizeof(listint_t));
-	if (node != NULL)
-	{
-		node->n = n;
-		node->next = *head;
-	}
-	else
-		return (NULL);
-	if (*head != NULL)
-		node->next = *head;
+	listint_t *node = malloc(sizeof(listint_t));
+	if (node != 0)
+{
+	node->n = n;
+	node->next = *head;
 	*head = node;
 	return (node);
+}
+	return (0);
 }

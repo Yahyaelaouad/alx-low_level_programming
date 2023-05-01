a0c8685cc818310a259e62339dd372e7f7dccca2
Yahyaelaouad <yahyaelaouad0@gmail.com git config --global user.email yahyaelaouad0@gmail.com>
2023-05-01 11:10:27 -0700

    TASK 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,9 +8,9 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
-	size_t count = NULL;
+	size_t count = 0;
 
-	while (cursor != 0)
+	while (cursor != NULL)
 	{
 		printf("%d\n", cursor->n);
 		count += 1;

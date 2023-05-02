61ce6e445f040cf586b1598f9817cdc861f6a0c3
Yahyaelaouad <yahyaelaouad0@gmail.com git config --global user.email yahyaelaouad0@gmail.com>
2023-05-02 16:06:59 -0700

    TASK 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - free the linked list
- * @head : pointer to list
- * Return: void
+ * free_listint2 - Frees a listint_t list.
+ * @head: pointer to the listint_t list to be freed
+ *
+ * Description: sets the head to NULL.
  */
 
 void free_listint2(listint_t **head)
@@ -16,8 +17,9 @@ void free_listint2(listint_t **head)
 	while (*head)
 	{
 		temp = (*head)->next;
-		*head = temp;
 		free(*head);
+		*head = temp;
 	}
-	head = NULL;
+
+	*head = NULL;
 }

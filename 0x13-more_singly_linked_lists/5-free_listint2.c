66c5d627283c2d5306d321eb86e8d7081f7b0e73
Yahyaelaouad <yahyaelaouad0@gmail.com git config --global user.email yahyaelaouad0@gmail.com>
2023-05-02 15:59:29 -0700

    TASK 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == 0)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -17,5 +17,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-	head = 0;
+	head = NULL;
 }

d24fb3f0d1bcdae5c8811915143616bb048e720f
Yahyaelaouad <yahyaelaouad0@gmail.com git config --global user.email yahyaelaouad0@gmail.com>
2023-05-04 16:20:06 -0700

    TASK 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,15 +2,20 @@
 
 /**
  * get_endianness - checks the endianness
- * Return: 0 if big endian, 1 if little endian
  * OWNED BY YAHYA
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	int num = 1;
 	char *byte_pointer = (char *)&num;
+
 	if (*byte_pointer == 1)
-	return 1;
+	{
+	return (1);
+	}
 	else
-	return 0;
+	{
+	return (0);
+	}
 }

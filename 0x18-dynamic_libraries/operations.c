354a468d1aae706334d6fe03bd757b4acd7f9265
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-06-19 19:34:07 -0700

    Adv1

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,25 +1,65 @@
-int add(int a, int b) {
-    return a + b;
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference between a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
-int div(int a, int b) {
-    if (b == 0) {
-        return 0;  /* Handle division by zero error */
-    }
-    return a / b;
+/**
+ * div - divides two integers
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: quotient of a divided by b
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+		return (0);
+
+	return (a / b);
 }
 
-int mod(int a, int b) {
-    if (b == 0) {
-        return 0;  /* Handle division by zero error */
-    }
-    return a % b;
+/**
+ * mod - calculates the modulus of two integers
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: remainder of a divided by b
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+		return (0);
+
+	return (a % b);
 }
